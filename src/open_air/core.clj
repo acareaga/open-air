07b340f175e85da780bce4334737037d32f1e91a
acareaga <aaroncareaga@gmail.com>
2015-10-20 08:17:11 -0600

    Cleaned formatting.

--- src/open_air/core.clj
+++ src/open_air/core.clj
@@ -7,13 +7,18 @@
 (defn current-tracks-url []
   (str base-url (System/currentTimeMillis)))
 
-(current-tracks-url)
-
 (defn current-tracks []
   (json/parse-string
    (:body
     (http/get (current-tracks-url)))))
 
-(def sample-tracks (current-tracks))
+(defn format-track [t]
+  (select-keys t
+               ["album" "artist" "title" "start_time" "label" "label_num"]
+               ))
 
-(first sample-tracks)
+(defn format-tracks [tracks]
+  (map format-track tracks))
+
+(def sample-tracks (current-tracks))
+(first (format-tracks sample-tracks))
