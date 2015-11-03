cd8eb1e43eb93fb6ec08f2a6afa32c3bfea5f31b
acareaga <aaroncareaga@gmail.com>
2015-11-03 08:05:59 -0700

    ready to create playlist and add tracks

--- src/open_air/core.clj
+++ src/open_air/core.clj
@@ -22,21 +22,65 @@
 
 (def rdio-base-url "https://services.rdio.com/api/1/")
 
-# dev app key
-(defn consumer-key "z47detwiazasnfqqkvr2lxjvce")
-(defn consumer-secret "BDFsBoy1BII0FdqRJR7E3Q")
-(defn token "Bearer") # need to add oauth code
+(def consumer-key "z47detwiazasnfqqkvr2lxjvce")
+(def consumer-secret "BDFsBoy1BII0FdqRJR7E3Q")
+(def token "Bearer AAAAAWEAAAAAAk1MdwAAAABWOLoNVjlizQAAABp1YXhucHl0N3FiYnI3bnQ0bHllZTN0czZ4dRHPJ275of0XM3fbqq6WyqAIOS5rrz7e5h-zqsxpzNZx")
 
-(defn rdio-query [params]
-  (http/post
-    ( str rdio-base-url "search"
-    { :query-params { "query" query
+(defn rdio-query [query]
+  (first
+  (get-in
+  (json/parse-string
+    (:body
+      (http/post
+      (str rdio-base-url "search" )
+      {:query-params {"query" query
                       "method" "search"
                       "types" "track" }
-      :throw-exceptions false
-      :headers {"Authorization" token }}))
+        :throw-exceptions false
+        :headers {"Authorization" token }})))
+    ["result" "results"])))
 
+(defonce tracks (format-tracks (current-tracks)))
+
+(defn rdio-playlists []
+  (get-in
+    (json/parse-string
+     (:body
+       (http/post
+         (str rdio-base-url "getPlaylists")
+         {:headers {"Authorization" token}
+          :query-params {"method" "getPlaylists"}})))
+     ["result" "owned"]))
+
+(defn current-playlist-name []
+  (str
+   "Open Air "
+   (.format (java.text.SimpleDateFormat. "MMMM d, yyyy") (java.util.Date.))))
+
+(current-playlist-name)
+
+(defn current-playlist []
+  (first
+   (filter
+   (fn [p]
+     (= (current-playlist-name)
+        (get p "name")))
+   (rdio-playlists))))
+
+(defn todays-playlist []
+  (if-let [pl (current-playlist)]
+    pl
+    "To do - create the playlist"))
+
+(todays-playlist)
 
 ; 1 - find all recent tracks
 ; 2 - create a playlist for today
 ; 3 - add tracks for today
+
+; (filter (fn [x] (= 6 x)) (range 10))
+
+(let [a "Aaron" b "Horace" c "Dan"]
+  (println a)
+  (println b)
+  (println c))
